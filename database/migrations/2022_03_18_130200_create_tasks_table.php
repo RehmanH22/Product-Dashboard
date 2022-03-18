7cc934ee7ffa09782cab588e1fd128ca58d95cdf
Rehman <rehmanh289@gmail.com>
2022-03-18 13:24:10 +0000

    added description string to Task Migration file

--- database/migrations/2022_03_18_130200_create_tasks_table.php
+++ database/migrations/2022_03_18_130200_create_tasks_table.php
@@ -15,9 +15,9 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('description');
             $table->timestamps();
         });
-    }
 
     /**
      * Reverse the migrations.

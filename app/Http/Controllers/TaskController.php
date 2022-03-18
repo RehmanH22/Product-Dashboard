1271f27ff368a2285a817876d08b0c5cee65e881
Rehman <rehmanh289@gmail.com>
2022-03-18 13:15:39 +0000

    Added index function to TaskController

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -15,7 +15,9 @@ class TaskController extends Controller
      */
     public function index()
     {
-        //
+        $tasks = Task::paginate(10);
+
+        return view('tasks.index', compact('tasks'));
     }
 
     /**

cb2a84472c7d80a548d7c8c6145d100349b1ddd1
Rehman <rehmanh289@gmail.com>
2022-03-19 09:33:15 +0000

    Added to Destroy Function for ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -43,6 +43,8 @@ class ProductController extends Controller
 
     public function destroy(Product $product)
     {
-        //
+        $product->delete();
+
+        return redirect('/products');
     }
 }

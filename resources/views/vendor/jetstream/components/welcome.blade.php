7586c9919b80f4ef1b899e58822e7dc63be35957
Rehman <rehmanh289@gmail.com>
2022-03-19 10:31:10 +0000

    Fixed Dashboard Colours and incorrect model name spelling

--- resources/views/vendor/jetstream/components/welcome.blade.php
+++ resources/views/vendor/jetstream/components/welcome.blade.php
@@ -11,14 +11,14 @@
     </div>
 </div>
 
-<div class="flex items-top min-h-screen bg-gray-50 dark:bg-gray-900">
+<div class="flex items-top min-h-screen bg-gray-50 dark:bg-gray-100">
     <div style="width: 900px;" class="container max-w-full mx-auto pt-4">
         <h1 class="text-2xl font-bold mb-4">General Statistics</h1>
         <div class="grid gap-7 sm:grid-cols-2 lg:grid-cols-4">
             <div class="p-5 bg-white rounded shadow-sm">
                 <div class="text-base text-gray-400 ">Total Products</div>
                 <div class="flex items-center pt-1">
-                    <div class="text-2xl font-bold text-gray-900 ">{{ \App\Models\ProductT::count() }}</div>
+                    <div class="text-2xl font-bold text-gray-900 ">{{ \App\Models\Product::count() }}</div>
                 </div>
             </div>
             <div class="p-5 bg-white rounded shadow-sm">
@@ -45,25 +45,25 @@
             <div class="p-5 bg-white rounded shadow-sm">
                 <div class="text-base text-gray-400 ">Total Stock Cost</div>
                 <div class="flex items-center pt-1">
-                    <div class="text-2xl font-bold text-gray-900 ">£{{ \App\Models\ProductT::sum('price') }}</div>
+                    <div class="text-2xl font-bold text-gray-900 ">£{{ \App\Models\Product::sum('price') }}</div>
                 </div>
             </div>
             <div class="p-5 bg-white rounded shadow-sm">
                 <div class="text-base text-gray-400 ">Min Product Price</div>
                 <div class="flex items-center pt-1">
-                    <div class="text-2xl font-bold text-gray-900 ">£{{ \App\Models\ProductT::min('price') }}</div>
+                    <div class="text-2xl font-bold text-gray-900 ">£{{ \App\Models\Product::min('price') }}</div>
                 </div>
             </div>
             <div class="p-5 bg-white rounded shadow-sm">
                 <div class="text-base text-gray-400 ">Avg Product Price</div>
                 <div class="flex items-center pt-1">
-                    <div class="text-2xl font-bold text-gray-900 ">£{{ substr(\App\Models\ProductT::avg('price'),0, 5) }}</div>
+                    <div class="text-2xl font-bold text-gray-900 ">£{{ substr(\App\Models\Product::avg('price'),0, 5) }}</div>
                 </div>
             </div>
             <div class="p-5 bg-white rounded shadow-sm">
                 <div class="text-base text-gray-400 ">Max Product Price</div>
                 <div class="flex items-center pt-1">
-                    <div class="text-2xl font-bold text-gray-900 ">£{{ \App\Models\ProductT::max('price') }}</div>
+                    <div class="text-2xl font-bold text-gray-900 ">£{{ \App\Models\Product::max('price') }}</div>
                 </div>
             </div>
         </div>

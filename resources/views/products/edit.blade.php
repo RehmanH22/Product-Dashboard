e41c61cd4d763530a5d69b38b4cd0f021155dab3
Rehman <rehmanh289@gmail.com>
2022-03-19 09:57:51 +0000

    Created form to edit product details in edit.blade.php

---
+++ resources/views/products/edit.blade.php
@@ -0,0 +1,90 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Edit Product
+        </h2>
+    </x-slot>
+
+    <div>
+        <div class="max-w-4xl mx-auto py-10 sm:px-6 lg:px-8">
+            <div class="mt-5 md:mt-0 md:col-span-2">
+                <form action="/products/{{$product->id}}" method="POST">
+                    @csrf
+                    @method('PUT')
+                    <div class="shadow overflow-hidden sm:rounded-md">
+                        <div class="px-4 py-5 bg-white sm:p-6">
+                            <div class="grid grid-cols-6 gap-6">
+                                <div class="col-span-6 sm:col-span-3">
+                                    <label for="type" class="block text-sm font-medium text-gray-700">Type</label>
+                                    <select id="type" name="type" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                                        @if($product->type == 'Book')
+                                            <option value="{{ $product->type }}">Book</option>
+                                            <option value="{{ $product->type }}">CD</option>
+                                        @else
+                                            <option value="{{ $product->type }}">CD</option>
+                                            <option value="{{ $product->type }}">Book</option>
+                                        @endif
+                                    </select>
+                                </div>
+
+                                <div class="col-span-6">
+                                    <label for="title" class="block text-sm font-medium text-gray-700">Title</label>
+                                    <input type="text" name="title" id="title" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                           value="{{ $product->title }}">
+                                    @error('title')
+                                    <span class="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
+                                        {{$message}}
+                                    </span>
+                                    @enderror
+                                </div>
+
+                                <div class="col-span-6">
+                                    <label for="author" class="block text-sm font-medium text-gray-700">Author</label>
+                                    <input type="text" name="author" id="author" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                           value="{{ $product->author }}">
+                                    @error('author')
+                                    <span class="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
+                                        {{$message}}
+                                    </span>
+                                    @enderror
+                                </div>
+
+                                <div class="col-span-6 sm:col-span-6 lg:col-span-2">
+                                    @if($product->type == 'Book')
+                                        <label for="other" class="block text-sm font-medium text-gray-700">Page Length</label>
+                                    @else
+                                        <label for="other" class="block text-sm font-medium text-gray-700">Play Length</label>
+                                    @endif
+                                    <input type="text" name="other" id="other" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                           value="{{ $product->other }}">
+
+                                    @error('other')
+                                    <span class="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
+                                        {{$message}}
+                                    </span>
+                                    @enderror
+                                </div>
+
+                                <div class="col-span-6 sm:col-span-6 lg:col-span-2">
+                                    <label for="price" class="block text-sm font-medium text-gray-700">Price</label>
+                                    <input type="text" name="price" id="price" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                           value="{{ $product->price }}">
+                                    @error('price')
+                                    <span class="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
+                                        {{$message}}
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+                        </div>
+                        <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                            <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                Save
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</x-app-layout>

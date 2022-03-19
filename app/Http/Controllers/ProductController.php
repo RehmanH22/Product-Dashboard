bd692f7f12400c5360d0e519102c2bcf15b1e2c9
Rehman <rehmanh289@gmail.com>
2022-03-19 09:32:26 +0000

    Added to Show Function for ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -28,7 +28,7 @@ class ProductController extends Controller
 
     public function show(Product $product)
     {
-        //
+        return view('products.show', compact('product'));
     }
 
     public function edit(Product $product)

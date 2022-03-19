6e41948e38d8595d73af4dfe285005e8ae14e523
Rehman <rehmanh289@gmail.com>
2022-03-19 10:00:50 +0000

    Changed button name to make it more easy to understand

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -8,7 +8,7 @@
     <div>
         <div class="max-w-6xl mx-auto py-10 sm:px-6 lg:px-8">
             <div class="block mb-8">
-                <a href="/products" class="bg-gray-200 hover:bg-gray-300 text-black font-bold py-2 px-4 rounded">Back to list</a>
+                <a href="/products" class="bg-gray-200 hover:bg-gray-300 text-black font-bold py-2 px-4 rounded">Go Back</a>
             </div>
             <div class="flex flex-col">
                 <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
